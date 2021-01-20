c4ce9d3c3a7ffc5a526015265b31ea21f2697a74
Matthieu Baumann <matthieu.baumann@astro.unistra.fr>
2021-01-20 16:21:07 +0100

    update to nom 6.0.1

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,7 @@ pub enum Error<'a> {
     CardSizeNotRespected(usize),
     MandatoryKeywordMissing(Cow<'a, str>),
     MustBe8BytesLong(&'a [u8]),
-    NomError(nom::Err<(&'a [u8], nom::error::ErrorKind)>),
+    NomError(nom::Err<nom::error::Error<&'a [u8]>>),
     SimpleKeywordBadValue,
     BitpixBadValue,
     NaxisBadValue,
@@ -15,8 +15,8 @@ pub enum Error<'a> {
     NegativeOrNullNaxisSize(usize),
 }
 
-impl<'a> From<nom::Err<(&'a [u8], nom::error::ErrorKind)>> for Error<'a> {
-    fn from(nom_err: nom::Err<(&'a [u8], nom::error::ErrorKind)>) -> Self {
+impl<'a> From<nom::Err<nom::error::Error<&'a [u8]>>> for Error<'a> {
+    fn from(nom_err: nom::Err<nom::error::Error<&'a [u8]>>) -> Self {
         Error::NomError(nom_err)
     }
 }
