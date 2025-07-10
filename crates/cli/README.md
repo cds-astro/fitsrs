3cacbefd35c0f7c030c1ab5d3adb2d355bf3c2e9
fxpineau <francois-xavier.pineau@astro.unistra.fr>
2025-07-10 17:46:35 +0200

    Create cli

---
+++ crates/cli/README.md
@@ -0,0 +1,9 @@
+# `fits-cli`
+
+Perform FITS file related operations from the command line.
+
+## About
+
+This **C**ommand **L**ine **I**nterface (CLI) is made from the
+[CDS FITS Rust library](https://github.com/cds-astro/fitsrs).
+
---
+++ crates/cli/src/main.rs
@@ -0,0 +1,36 @@
+use std::{
+    error::Error,
+};
+
+use clap::Parser;
+
+use fits_cli::r#struct::Struct;
+
+// Avoid musl's default allocator due to lackluster performance
+// https://nickb.dev/blog/default-musl-allocator-considered-harmful-to-performance
+#[cfg(all(target_env = "musl", target_arch = "x86_64"))]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+/// Perform FITS file related operations on the command line.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+enum Args {
+    /// Read and print the structure of a FITS file
+    #[clap(name = "struct")]
+    Struct(Struct),
+}
+
+impl Args {
+    fn exec(self) -> Result<(), Box<dyn Error>> {
+        match self {
+            Self::Struct(args) => args.exec(),
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+    let args = Args::parse();
+    args.exec()
+}
